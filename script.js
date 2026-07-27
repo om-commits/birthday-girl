f5e0c5c9f6306ce6fa323e3f6b531afea7673809
om-commits <67972624+om-commits@users.noreply.github.com>
2026-07-27 14:33:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -383,21 +383,42 @@ const slideImage=document.getElementById("slideImage");
 
 const slides=[
 
-"images/1.jpg",
+"1.jpg",
 
-"images/2.jpg",
+"2.jpg",
 
-"images/3.jpg",
+"3.jpg",
 
-"images/4.jpg",
+"4.jpg",
 
-"images/5.jpg",
+"5.jpg",
 
-"images/6.jpg",
+"6.jpg",
 
-"images/7.jpg",
+"7.jpg",
 
-"images/8.jpg"
+"8.jpg"
+
+"9.jpg"
+
+"10.jpg"
+
+"11.jpg"
+
+"12.jpg"
+
+"13.jpg"
+
+"14.jpg"
+
+"15.jpg"
+
+"16.jpg"
+
+"17.jpg"
+
+"18.jpg"
+
 
 ];
 
